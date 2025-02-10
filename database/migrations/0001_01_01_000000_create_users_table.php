80b4562e62aa7b11d462597318cbdab0712177d7
aisyaaww <aura.intan548@smk.belajar.id>
2025-02-10 21:06:42 +0700

    push tabel users dan categories

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username'); 
             $table->string('password');
-            $table->rememberToken();
+            $table->string('name'); 
+            $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->rememberToken();
+            $table->string('phone'); 
             $table->timestamps();
+
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
